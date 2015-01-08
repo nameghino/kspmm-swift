eef1e0f03381c3951553eec6bc920fd694b25b15
Nicolas Ameghino <nicolas.ameghino@globant.com>
2015-01-08 13:45:42 -0300

    Fixed parseJSONFile() method Added KSPMMStartType and KSPMMCommand enums Rewrote checkArguments() to return start type and args dict Program startup now depends on how it is started (with or without specific KSP directory)

--- kspmm/main.swift
+++ kspmm/main.swift
@@ -13,31 +13,97 @@ func getPath() -> NSURL? {
     return NSURL(fileURLWithPath: pathComponents)?.URLByDeletingLastPathComponent
 }
 
-func parseJSONFile(url: NSURL) -> (NSError?, [String:AnyObject]) {
-    let data = NSData(contentsOfURL: url)
-    var error: NSError?
-    let object = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: &error) as [String:AnyObject]
-    return (error, object)
+func parseJSONFile(url: NSURL) -> (NSError?, [String:AnyObject]?) {
+    if let data = NSData(contentsOfURL: url) {
+        var error: NSError?
+        var object: [String: AnyObject]? = nil
+        if let o = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: &error) as? [String: AnyObject] {
+            object = o as [String: AnyObject]
+        } else if let e = error {
+            println("error: \(e.localizedDescription)")
+        }
+        return (error, object)
+    } else {
+        
+        //    init(domain: String, code: Int, userInfo dict: [NSObject : AnyObject]?)
+
+        let error = NSError(domain: NSCocoaErrorDomain,
+            code: 206,
+            userInfo: [NSLocalizedDescriptionKey: "file not found"])
+        return (error, nil)
+    }
 }
 
-func checkArguments(args: [AnyObject]) -> Bool {
-    return args.count > 2
+enum KSPMMStartType {
+    case KSPMMSpecificKSPInstance
+    case KSPMMLocalKSPInstance
+    case KSPMMNotEnoughArguments
 }
+
+enum KSPMMCommand {
+    case Install
+    case List
+    case Remove
+    case RemoveAll
+}
+
+func checkArguments(args: [AnyObject]) -> (KSPMMStartType, [String:String]) {
+    let t = args as [String]
+    if args.count == 3 {
+        return (
+            KSPMMStartType.KSPMMLocalKSPInstance,
+            [
+                "Command": t[1],
+                "ModFile": t[2]
+            ]
+        )
+    } else if args.count == 4 {
+        return (
+            KSPMMStartType.KSPMMSpecificKSPInstance,
+            [
+                "KSPDirectory": t[1],
+                "Command": t[2],
+                "ModFile": t[3]
+            ]
+        )
+    }
+    return (.KSPMMNotEnoughArguments, [:])
+}
+
+
 
 // program
 
-let validArgs = checkArguments(NSProcessInfo.processInfo().arguments)
-if (validArgs) {
-    println("you need to provide the mod file you want to install")
-    exit(1)
+// test args
+// 1- /Users/install/tmp/KSP_osx
+// 2- install
+// 3- /Users/install/tmp/Neophytes_Elementary_Aerodynamics_Replacement-v1.3.1.zip
+
+
+let (startType, parsedArgs): (KSPMMStartType, [String:String]) = checkArguments(NSProcessInfo.processInfo().arguments)
+
+if (startType == .KSPMMNotEnoughArguments) {
+    println("not enough arguments specified")
+    exit(EXIT_FAILURE)
 }
 
-let zipPath = NSProcessInfo.processInfo().arguments[1] as String
+let zipPath = parsedArgs["ModFile"] as String!
 let zipURL = NSURL(fileURLWithPath: zipPath.stringByStandardizingPath)!
-let fakemod = KSPMod(url: zipURL)
+let mod = KSPMod(url: zipURL)
+let files = mod.gamedataFiles()
+println("files: \(files)")
 
-let kspPath = "/Users/nameghino/Desktop/KSP_osx"
-let kspURL = NSURL(fileURLWithPath: kspPath)!
-if let processor = KSPProcessor(targetDirectory: kspURL) {
-    println("list of installed mods: \(processor.installedMods())")
+//FIXME: makes no sense at all... effin' swift
+var kspPath: String = ""
+if (startType == .KSPMMSpecificKSPInstance) {
+    kspPath = parsedArgs["KSPDirectory"] as String!
+} else {
+    kspPath = "."
 }
+let kspURL = NSURL(fileURLWithPath: kspPath)!
+
+if let processor = KSPProcessor(targetDirectory:kspURL) {
+    //processor.installMod(mod)
+}
+
+
