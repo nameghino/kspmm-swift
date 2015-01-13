42c058110b4f31d8e39a14d5d7709f80bea1d710
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-01-12 22:53:16 -0300

    Added empty extension just in case

---
+++ kspmm/Array+SwiftShouldHaveThis.swift
@@ -0,0 +1,12 @@
+//
+//  Array+SwiftShouldHaveThis.swift
+//  kspmm
+//
+//  Created by Nicolas Ameghino on 1/12/15.
+//  Copyright (c) 2015 Nicolas Ameghino. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+}
